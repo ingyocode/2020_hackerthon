ce36a25f0d20322ea2c3d10ce79dd37dc4293e0a
Tkendi <acg70159@gmail.com>
2020-09-21 13:44:13 +0900

    error upload

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,6 +9,27 @@ class App extends React.Component {
       </div>
     );
   }
+=======
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Testing   
+        </a>
+      </header>
+    </div>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App
