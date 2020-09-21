87404fca1980ce30baff3dc91b35a57206c238bf
Tkendi <acg70159@gmail.com>
2020-09-21 20:03:51 +0900

    edit app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {Button} from '@material-ui/core';
+import React from "react";
+import { Button } from "@material-ui/core";
 
 function App() {
   return (
     <Button variant="contained" color="primary">
       Hello World
     </Button>
-  )
+  );
 }
 
-export default App  
+export default App;
