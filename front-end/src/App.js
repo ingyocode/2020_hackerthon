8c594d3dbe5fbd6a81236460f930cc4eb8b356c4
Tkendi <acg70159@gmail.com>
2020-09-21 19:00:51 +0900

    app edit error

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import {Button} from '@material-ui/core';
 
 function App() {
   return (
