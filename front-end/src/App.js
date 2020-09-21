3ee703c6abc1ccd9986cd77ad07ad3d436dd1267
Tkendi <acg70159@gmail.com>
2020-09-21 13:50:49 +0900

    testing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
 
-<<<<<<< Updated upstream
 class App extends React.Component {
   render() {
     return (
@@ -10,27 +9,6 @@ class App extends React.Component {
       </div>
     );
   }
-=======
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Testing   
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> Stashed changes
 }
 
 export default App
